a1c704fa67c6f8fe30f8423bc144de487bdfccf3
bododge <bo@iapps24.com>
2012-11-28 00:36:35 -0600

    Static Website
    to be extended for front end

---
+++ includes/jquery.ui.totop.js
@@ -0,0 +1,93 @@
+/*
+|--------------------------------------------------------------------------
+| UItoTop jQuery Plugin 1.1
+| http://www.mattvarone.com/web-design/uitotop-jquery-plugin/
+|--------------------------------------------------------------------------
+*/
+
+(function($){
+	$.fn.UItoTop = function(options) {
+
+ 		var defaults = {
+			text: 'To Top',
+			min: 200,
+			inDelay:600,
+			outDelay:400,
+  			containerID: 'toTop',
+			containerHoverID: 'toTopHover',
+			scrollSpeed: 1200,
+			easingType: 'linear'
+ 		};
+
+ 		var settings = $.extend(defaults, options);
+		var containerIDhash = '#' + settings.containerID;
+		var containerHoverIDHash = '#'+settings.containerHoverID;
+		
+		$('body').append('<a href="#" id="'+settings.containerID+'">'+settings.text+'</a>');
+		$(containerIDhash).hide().click(function(){
+			$('html, body').animate({scrollTop:0}, settings.scrollSpeed, settings.easingType);
+			$('#'+settings.containerHoverID, this).stop().animate({'opacity': 0 }, settings.inDelay, settings.easingType);
+			return false;
+		})
+		.prepend('<span id="'+settings.containerHoverID+'"></span>')
+		.hover(function() {
+				$(containerHoverIDHash, this).stop().animate({
+					'opacity': 1
+				}, 600, 'linear');
+			}, function() { 
+				$(containerHoverIDHash, this).stop().animate({
+					'opacity': 0
+				}, 700, 'linear');
+			});
+					
+		$(window).scroll(function() {
+			var sd = $(window).scrollTop();
+			if(typeof document.body.style.maxHeight === "undefined") {
+				$(containerIDhash).css({
+					'position': 'absolute',
+					'top': $(window).scrollTop() + $(window).height() - 50
+				});
+			}
+			if ( sd > settings.min ) 
+				$(containerIDhash).fadeIn(settings.inDelay);
+			else 
+				$(containerIDhash).fadeOut(settings.Outdelay);
+		});
+
+};
+})(jQuery);
+
+
+
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Montserrat+Alternates::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
+  
+  
+  
+  <script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Dosis::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
+  
+  
+  
